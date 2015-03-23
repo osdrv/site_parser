9628a5d77aef00c621c8f09ea5a6bce6d4337d80
4pcbr <me@4pcbr.com>
2015-03-24 00:15:52 +0100

    Added recipients list

--- whatsnew.rb
+++ whatsnew.rb
@@ -2,6 +2,7 @@
 
 $LOAD_PATH.unshift File.dirname(__FILE__)
 
+require 'digest/sha1'
 require 'logger'
 require 'em-synchrony'
 require 'em-synchrony/em-http'
@@ -9,19 +10,27 @@ require 'em-synchrony/fiber_iterator'
 require 'nokogiri'
 require 'funda_listing_parser'
 
-SOURCES             = 'SOURCES'
-LOCK                = 'LOCK'
-LAST_RUN            = 'LAST_RUN'
+SOURCES    = 'SOURCES'
+RECIPIENTS = 'RECIPIENTS'
+LOCK       = 'LOCK'
+LAST_RUN   = 'LAST_RUN'
 
 $log = nil
 
 def parse_response(response)
   doc = Nokogiri::HTML(response)
   elements = doc.css(FundaListingParser.listing_element_css)
+  res = []
   elements.each do |element|
     parsed_obj = FundaListingParser.parse_slop(element)
-    $log.debug parsed_obj
+    res.push parsed_obj
   end
+  
+  res
+end
+
+def notify_if_new(channel, items)
+  #TODO
 end
 
 def main
@@ -32,9 +41,20 @@ def main
   $log.info 'Reading the sources file'
   
   sources = []
-  File.new(SOURCES, 'r').each_line do |line|
+  File.new(SOURCES, 'r').each_line { |line|
     sources.push line
-  end
+  }.close
+  
+  sources = sources.map { |s| s.strip }
+  $log.debug "Sources: #{sources}"
+  
+  recipients = []
+  File.new(RECIPIENTS, 'r').each_line { |line|
+    recipients.push line
+  }.close
+  
+  recipients = recipients.map { |r| r.strip }
+  $log.debug "Recipients: #{recipients}"
   
   EM.synchrony do
     
@@ -45,7 +65,7 @@ def main
     
     EM::Synchrony::FiberIterator.new(sources, concurrency).each do |src|
       response = EM::HttpRequest.new(src).get.response
-      parse_response response
+      notify_if_new(Digest::SHA1.hexdigest(src), parse_response(response))
     end
 
     $log.info "Done."
